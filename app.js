806e6419d73931ce8e6808d2d27bf7d7c1211af4
dimanvaz <dimanvaz04@gmail.com>
2022-01-29 01:39:14 +0300

    fix saveOrder(wrote manually)

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ exP.get("/",function (req,res){
         );
     });
     Promise.all([cat, catDescription]).then(function (value) {
-        console.log(value[1]);
+        //console.log(value[1]);
         res.render('shop', {
             goods: JSON.parse(JSON.stringify(value[0])),
             cat: JSON.parse(JSON.stringify(value[1])),
@@ -92,7 +92,7 @@ exP.get('/item',function(request,response){
     let id = request.query.id || 1;
     conn.query("SELECT * FROM goods WHERE id="+id, function (err,result, fields) {
         if(err) throw err
-        console.log('result from axp get item',result);
+        //console.log('result from axp get item',result);
         response.render('item',{
             item: JSON.parse(JSON.stringify(result)),
         })
@@ -136,7 +136,7 @@ exP.post('/finish-order',(request, response)=>{
             'SELECT id,name,cost FROM goods WHERE id IN (' + key.join(',') + ')',
             function (error, result, fields) {
                 if (error) throw error;
-                console.log('result from finish-order = ', result);
+                //console.log('result from finish-order = ', result);
                 sendMail(request.body, result).catch(console.error);
                 saveOrder(request.body, result);
                 response.send('1');
@@ -148,7 +148,26 @@ exP.post('/finish-order',(request, response)=>{
 //data = req.body = данные заказчика из формы + id/count из cart
 //result / positionsList = ответ с базы данных по запросу = товары, найденные по id
 function saveOrder(data, result) {
+    //1 - в юзер-таблицу
+    let {username,phone,address,email,key} = data;
+    let usersQuery = "INSERT INTO user_info (user_name,user_phone,user_email,address) VALUES (" +
+                    `"${username}", "${phone}", "${address}", "${email}"` + ")";
+    conn.query(usersQuery, (err,result)=>{
+        if(err) throw err;
+        console.log('1 user wrote to DB : \n', result);
+    });
+    //2 - в гудс таблицу
+    let date = new Date()/1000;
+    result.forEach(item => {
+      let shopOrderQuery =
+        "INSERT INTO shop_order (date, user_id, goods_id, goods_cost, goods_amount, total) VALUES (" +
+       `"${date}", "12", "${item.id}", "${item.cost}", "${key[item.id]}", "${key[item.id] * item.cost}"` + ")";
 
+        conn.query(shopOrderQuery, (err,result)=>{
+            if(err) throw err;
+            console.log('--------- \n 1 order recorded to DB: \n',result);
+        })
+    })
 }
 
 //в sendMail передаются те же данные, что и в saveOrder
